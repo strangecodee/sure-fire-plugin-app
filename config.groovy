d9bce9b5021a2f8cf2828c4f1db985915eaf458e
strangecodee <anuragmaurya883@gmail.com>
2026-05-25 20:51:47 +0530

    Implemented Jenkins Ansible shared library with modular deployment workflow

---
+++ config.groovy
@@ -0,0 +1,17 @@
+[
+    REPO_URL : 'https://github.com/strangecodee/sure-fire-plugin-app.git',
+
+    BRANCH : 'dev',
+
+    SLACK_CHANNEL_NAME : '#build-status',
+
+    ENVIRONMENT : 'prod',
+
+    INVENTORY : 'ansible/inventory/prod.ini',
+
+    PLAYBOOK : 'ansible/deploy.yml',
+
+    ACTION_MESSAGE : 'Application Successfully Deployed',
+
+    KEEP_APPROVAL_STAGE : true
+]
